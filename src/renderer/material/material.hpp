941094c3f2ac5fb8d6ae5dc2d70ef4e1dae01a71
Michael Tsukanov <tsukanovWOW@ya.ru>
2024-12-15 16:17:59 +0300

    Handle lack of std::vector<T>::append_range

--- src/renderer/material/material.hpp
+++ src/renderer/material/material.hpp
@@ -10,8 +10,8 @@ namespace mr {
     mr::UniformBuffer _ubo;
     mr::Shader _shader;
 
-    mr::DescriptorAllocator _descriptor_allocator; 
-    mr::DescriptorSet _descriptor_set; 
+    mr::DescriptorAllocator _descriptor_allocator;
+    mr::DescriptorSet _descriptor_set;
     mr::GraphicsPipeline _pipeline;
 
     std::array<vk::VertexInputAttributeDescription, 3> _descrs {
@@ -84,7 +84,11 @@ namespace mr {
 
     MaterialBuilder & add_value(const auto &value)
     {
+#ifdef __cpp_lib_containers_ranges
       _ubo_data.append_range(std::span {(float *)&value, sizeof(value)});
+#else
+      _ubo_data.insert(_ubo_data.end(), (float *)&value, (float *)(&value + 1));
+#endif
       return *this;
     }
 
@@ -116,4 +120,4 @@ namespace mr {
   };
 } // namespace mr
 
-#endif __material_cpp_
+#endif // __material_cpp_
