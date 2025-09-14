05777830fd2ce6e22b6839074e75d9506fe26807
Michael Tsukanov <tsukanovWOW@ya.ru>
2025-09-15 00:16:19 +0300

    Pre-release cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,18 @@
 
 int main(int argc, const char **argv)
 {
+  if (argc != 2) {
+    MR_ERROR("Usage: model-renderer /path/to/model.gltf");
+    exit(1);
+  }
+
   mr::Application app;
 
   auto render_context = app.create_render_context({1920, 1080});
   auto scene = render_context->create_scene();
   scene->create_directional_light(mr::Norm3f(1, 1, -1));
   // scene->create_directional_light(mr::Norm3f(-1, 1, 1));
-  scene->create_model("ABeautifulGame/ABeautifulGame.gltf");
+  scene->create_model(argv[1]);
 
   // auto file_writer = render_context->create_file_writer();
   // app.render_frames(*render_context, scene, file_writer, "frame", 10);
