fd183baea775f68ceb228410109cb98efcf2c275
Dmitriy Kozenko <8863314@gmail.com>
2025-12-10 00:49:06 +0300

    continue working

--- bin/shaders/culling/late_instances_culling/shader.comp.glsl
+++ bin/shaders/culling/late_instances_culling/shader.comp.glsl
@@ -80,6 +80,21 @@ layout(set = BINDLESS_SET, binding = STORAGE_IMAGES_BINDING, r32f) uniform image
 layout(set = BINDLESS_SET, binding = TEXTURES_BINDING) uniform sampler2D SampledStorageImages[];
 #define DepthPyramid SampledStorageImages[buffers_data.depth_pyramid_image_id]
 
+//   vec4 rectangle_inv = rectangle;
+//   rectangle_inv.y = buffers_data.depth_pyramid_size.y - rectangle_inv.w;
+//   rectangle_inv.w = buffers_data.depth_pyramid_size.y - rectangle_inv.y;
+//
+//   float rectangle_width_inv = rectangle_inv.z - rectangle_inv.x;
+//   float rectangle_height_inv = rectangle_inv.w - rectangle_inv.y;
+//   vec2 rectangle_center_inv = vec2(rectangle_inv.x + rectangle_width_inv / 2, rectangle_inv.y + rectangle_height_inv / 2);
+//   float level_inv = floor(log2(max(rectangle_width_inv, rectangle_height_inv)));
+//   float old_depth_inv = textureLod(DepthPyramid, rectangle_center_inv, level_inv).r;
+//   float old_depth = max(old_depth1, old_depth_inv);
+
+  // int ilevel = int(level);
+  // vec2 level_rectangle_center = rectangle_center / pow(2, ilevel);
+  // float old_depth = imageLoad(DepthLevel(ilevel), ivec2(level_rectangle_center)).r;
+
 void main()
 {
   uint id = gl_LocalInvocationID.x + gl_WorkGroupID.x * THREADS_NUM;
@@ -107,42 +122,47 @@ void main()
 
   // --- Get current depth of already visible objects ---
   vec4 rectangle_screen = get_bound_box_screen_rectangle(bb, camera_buffer.vp);
-  vec4 rectangle;
-  // TODO(dk6): maybe here correct '*= -1' y components
+  // Flip over Ox
+  float tmp = -rectangle_screen.y;
+  rectangle_screen.y = -rectangle_screen.w;
+  rectangle_screen.w = tmp;
+
   // rectangle now in [-1; 1] screen coords, not in pixels - we must convert it to [0; w] and [0; h]
+  vec4 rectangle;
   rectangle.x = ((rectangle_screen.x + 1) / 2) * buffers_data.depth_pyramid_size.x;
   rectangle.y = ((rectangle_screen.y + 1) / 2) * buffers_data.depth_pyramid_size.y;
   rectangle.z = ((rectangle_screen.z + 1) / 2) * buffers_data.depth_pyramid_size.x;
   rectangle.w = ((rectangle_screen.w + 1) / 2) * buffers_data.depth_pyramid_size.y;
 
+  // rectangle.y = buffers_data.depth_pyramid_size.y - rectangle.w;
+  // rectangle.w = buffers_data.depth_pyramid_size.y - rectangle.y;
+
   float rectangle_width = rectangle.z - rectangle.x;
   float rectangle_height = rectangle.w - rectangle.y;
   vec2 rectangle_center = vec2(rectangle.x + rectangle_width / 2, rectangle.y + rectangle_height / 2);
   float level = floor(log2(max(rectangle_width, rectangle_height)));
-  // level = min(level, int(buffers_data.mip_levels_number) - 1);
 
-  // int ilevel = int(level);
-  // vec2 level_rectangle_center = rectangle_center / pow(2, ilevel);
-  // float old_depth = imageLoad(DepthLevel(ilevel), ivec2(level_rectangle_center)).r;
-  float old_depth = textureLod(DepthPyramid, ivec2(rectangle_center), level).r;
+  float old_depth = textureLod(DepthPyramid, rectangle_center, level).r;
 
   // --- Get current depth closest to cam point of bound box ---
   vec3 bb_center = (bb.min.xyz + bb.max.xyz) / 2;
-  vec3 dir_to_cam = normalize(camera_buffer.pos.xyz - bb_center);
+  vec3 dir_to_cam = camera_buffer.pos.xyz - bb_center;
+  float dist_to_cam = length(dir_to_cam);
+  dir_to_cam = normalize(dir_to_cam);
   float bb_size = length(bb.max.xyz - bb.min.xyz) / 2;
-  vec3 closest_bb_point = bb_center + dir_to_cam * bb_size;
+  float optimal_len = min(bb_size, dist_to_cam * 0.99);
+  vec3 closest_bb_point = bb_center + dir_to_cam * optimal_len;
   vec4 projected = (camera_buffer.vp * vec4(closest_bb_point, 1));
   float new_depth = projected.z / projected.w;
 
   // --- Check visibility ---
   float bias = 0.001;
-  // TODO(dk6): it is better to more correct build closest_point - make it always between camera
-  new_depth = min(new_depth, 0.9999);
-  // TODO(dk6): sometimes old_depth readed as 1.00 -> maybe replace max -> min in building depth pyramid
+  // new_depth = min(new_depth, 0.9999);
   bool visible = new_depth < old_depth + bias;
 
+  bool was_visible = instance_data.visible_last_frame == 1;
   instances_datas[id].visible_last_frame = visible ? 1 : 0;
-  if (!visible) {
+  if (!visible || was_visible) {
     return;
   }
 
