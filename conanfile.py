a5aa8f43aa030baf62c3ed3ef4373e3a5a4d186d
Dmitriy Kozenko <8863314@gmail.com>
2026-03-16 22:09:46 +0300

    Updated tracy

--- conanfile.py
+++ conanfile.py
@@ -38,7 +38,7 @@ class MrGraphicsRecipe(ConanFile):
 
         self.requires("onetbb/2022.2.0")
 
-        self.requires("tracy/0.12.2")
+        self.requires("tracy/0.13.1")
 
     def build_requirements(self):
         self.tool_requires("cmake/[>3.26]")
